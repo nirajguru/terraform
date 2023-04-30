c94a31a52ad3856d853bc54f6e310c19bcba7f18
Niraj G <niraj.guru@gmail.com>
2023-04-30 20:10:33 +0000

    added eks role

---
+++ aws/us-east-1/eks/iam/roles.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = "arn:aws:iam:525352990358:aws:policy/AmazonEKSClusterPolicy"
+
+  tags = {
+    name = "eks-assume-role-niraj",
+    project = "trying-eks-terraform"
+  }
+}
